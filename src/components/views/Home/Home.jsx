47ee004939fe3d72b976e3782240cb4e802a8004
SantiRosso <santiagorosso@live.com.ar>
2023-06-22 15:18:37 -0300

    Broker, Balance, Equity

--- src/components/views/Home/Home.jsx
+++ src/components/views/Home/Home.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import axios from "axios";
 
@@ -7,23 +7,26 @@ const Home = () => {
     const [info, setInfo] = useState()
 
     const getInfo = async () => {
+        console.log(await axios.get("https://fx-auto-trade-1c1l.onrender.com/information"))
         setInfo(
             await axios.get("https://fx-auto-trade-1c1l.onrender.com/information")
         )
     }
 
-    getInfo()
+    useEffect(()=> {
+        getInfo()
+    },[])
 
-    console.log(info ? info : "nada")
+    console.log(info?.data ? info.data : "nada")
     return(
         <div>
             <h1>Home</h1>
             <h3>Broker:</h3>
-            <h5>{info?.broker}</h5>
+            <h5>{info?.data?.broker}</h5>
             <h3>Balance:</h3>
-            <h5>{info?.balance}</h5>
+            <h5>{info?.data?.balance}</h5>
             <h3>Equity:</h3>
-            <h5>{info?.equity}</h5>
+            <h5>{info?.data?.equity}</h5>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./app.js";
 import { BrowserRouter } from "react-router-dom";
+// import axios from "axios";
+
+// axios.defaults.baseURL = process.env.REACT_APP_API;
 
 ReactDOM.render(
   <React.StrictMode>
